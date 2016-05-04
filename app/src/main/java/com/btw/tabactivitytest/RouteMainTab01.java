81f471c1568e81c585d24fc8b1e75888229dadbf
won <wenwenwen888@live.com>
2016-05-04 18:22:49 +0800

    模仿微信三个Tab的Demo

---
+++ app/src/main/java/com/btw/tabactivitytest/RouteMainTab01.java
@@ -0,0 +1,17 @@
+package com.btw.tabactivitytest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Won on 2016/5/4.
+ */
+public class RouteMainTab01 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.route_tab_01, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/btw/tabactivitytest/StationMainTab02.java
@@ -0,0 +1,17 @@
+package com.btw.tabactivitytest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Won on 2016/5/4.
+ */
+public class StationMainTab02 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.station_tab_02, container, false);
+        return view;
+    }
+}
