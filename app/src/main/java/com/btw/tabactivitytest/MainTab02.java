67fcefcd9969564aea94d59cacc1a8e74f89d245
Won <wenwenwen888@live.com>
2016-09-20 09:55:23 +0800

    add lazyFragment

--- app/src/main/java/com/btw/tabactivitytest/MainTab02.java
+++ app/src/main/java/com/btw/tabactivitytest/MainTab02.java
@@ -1,17 +1,45 @@
 package com.btw.tabactivitytest;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by Won on 2016/5/4.
  */
-public class MainTab02 extends Fragment {
+public class MainTab02 extends LazyFragment {
+
+    private View view;
+    // 标志位，标志已经初始化完成。
+    private boolean isPrepared;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.tab_02, container, false);
+        view = inflater.inflate(R.layout.tab_02, container, false);
+
+        initUI();//实例化控件
+        isPrepared = true;
+        lazyLoad();//加载数据
+
         return view;
+    }
+
+    /**
+     * 实例化组件
+     */
+    private void initUI() {
+
+    }
+
+    /**
+     * 实现懒加载,当屏幕显示这个界面的时候才会触发次方法
+     */
+    @Override
+    protected void lazyLoad() {
+        if (isPrepared && isVisible) {
+            //TODO 在这里处理逻辑
+            Toast.makeText(getContext(), "MainTab02", Toast.LENGTH_SHORT).show();
+        }
     }
 }
